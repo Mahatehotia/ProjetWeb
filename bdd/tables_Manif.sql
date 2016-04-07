9f9bf29e7b6ce32b32228d30d174d3ea4343ad47
Pascal P <pascal.phelipot@gmail.com>
2016-04-07 08:04:51 +0200

    ajout de client pour la gestion de droits

--- bdd/tables_Manif.sql
+++ bdd/tables_Manif.sql
@@ -1,4 +1,4 @@
-DROP TABLE  IF EXISTS paniers,commandes, users, etats, manifestant, typeManifestants;
+DROP TABLE  IF EXISTS paniers,commandes, manifestant, typeManifestants, client;
 
 CREATE TABLE IF NOT EXISTS typeManifestants (
   id int(10) NOT NULL,
@@ -26,3 +26,16 @@ INSERT INTO manifestant (id,typeManifestant,nom,prix,photo,stock) VALUES
 (1,2, 'Nazi','100','Nazi.jpeg',5),
 (2,2, 'Femen','5.5','Femen.jpeg',4),
 (3,1, 'Pacifiste','8.5','Pacifiste.jpeg',10);
+
+CREATE TABLE IF NOT EXISTS client (
+  id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  email VARCHAR(50),
+  mdp VARCHAR(32),
+  nom VARCHAR(25),
+  prenom VARCHAR(25),
+  droits ENUM ('admin', 'user')
+);
+
+INSERT INTO client (email, mdp, nom, prenom, droits) VALUES
+  ('pascal.phelipot@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Pascal', 'PHELIPOT', 'admin'),
+  ('tochna', '098f6bcd4621d373cade4e832627b4f6', 'Tiona', 'RALIJAONA', 'user');
