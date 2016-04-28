8c8edd9b68f92096d582813f9c9d73edcecb805c
Pascal P <pascal.phelipot@gmail.com>
2016-04-28 09:33:32 +0200

    Ajout squelette commande

---
+++ App/Controller/CommandeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pascal
+ * Date: 28/04/16
+ * Time: 09:29
+ */
+
+namespace App\Controller;
+
+
+class CommandeController
+{
+
+}
---
+++ App/Model/CommandeModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pascal
+ * Date: 28/04/16
+ * Time: 09:30
+ */
+
+namespace App\Model;
+
+
+class CommandeModel
+{
+
+}
